6a46c9b38245900e009050f4c8901e88b8064856
creek0810 <creek880810@gmail.com>
2019-05-30 22:34:26 +0800

    Initial Commit

---
+++ 2019_ios_finalTests/_019_ios_finalTests.swift
@@ -0,0 +1,34 @@
+//
+//  _019_ios_finalTests.swift
+//  2019_ios_finalTests
+//
+//  Created by 王心妤 on 2019/5/30.
+//  Copyright © 2019 river. All rights reserved.
+//
+
+import XCTest
+@testable import _019_ios_final
+
+class _019_ios_finalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
